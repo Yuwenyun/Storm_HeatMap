1f57606faceab855426ce7a38341c902c1a4c369
yuwenyun <yuwenyun.owen.lee@gmail.com>
2017-08-03 22:14:09 +0800

    Update App.java

--- src/main/java/com/owen/HeatMap/App.java
+++ src/main/java/com/owen/HeatMap/App.java
@@ -2,6 +2,10 @@ package com.owen.HeatMap;
 
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.generated.AlreadyAliveException;
+import org.apache.storm.generated.AuthorizationException;
+import org.apache.storm.generated.InvalidTopologyException;
 import org.apache.storm.utils.Utils;
 
 /**
@@ -16,13 +20,25 @@ public class App
     public static void main( String[] args )
     {
         Config config = new Config();
-        config.setDebug(true);
+        config.putAll(Utils.readDefaultConfig());
+        config.put(Config.STORM_LOCAL_HOSTNAME, "localhost");
+        config.setNumWorkers(2);
+        config.setMessageTimeoutSecs(60);
         
-        LocalCluster cluster = new LocalCluster();
-        cluster.submitTopology("heat-map-topology", config, HeatMapTopologyBuilder.build());
+        try
+		{
+			StormSubmitter.submitTopology("heat-map-topology", config, HeatMapTopologyBuilder.build());
+		}
+		catch (AlreadyAliveException | InvalidTopologyException | AuthorizationException e)
+		{
+			e.printStackTrace();
+		}
         
-        Utils.sleep(100_000);
-        cluster.killTopology("heat-map-topology");
-        cluster.shutdown();
+//        LocalCluster cluster = new LocalCluster();
+//        cluster.submitTopology("heat-map-topology", config, HeatMapTopologyBuilder.build());
+//        
+//        Utils.sleep(100_000);
+//        cluster.killTopology("heat-map-topology");
+//        cluster.shutdown();
     }
 }
